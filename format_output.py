8ae7dd6450e093e1f46bad0a4f8435e9eec2098e
rh-song <songrh13354303@gmail.com>
2016-05-26 13:39:23 +0800

    chapter6-string

---
+++ format_output.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+# 16进制输出
+print "%x" % 108
+print "%#x" % 108
+
+# float
+print "%f" % 1234.567890
+print "%.2f" % 1234.567890
+print "%E" % 1234.567890
+print "%g" % 1234.567890
+
+# interger & string
+print "%+d" % 4
+print "%+d" % -4
+print "We are at %d%%" % 100
+print "Your host is: %s" % "earth"
+
+print r'\n'
