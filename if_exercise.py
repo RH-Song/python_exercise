eb8e59d423a94ba1f3c9e2f374d6f5a3b7a78157
songrh <song_rihui@qq.com>
2019-05-15 17:04:52 +0800

    add if and for and while

---
+++ if_exercise.py
@@ -0,0 +1,8 @@
+a = input()
+b = input()
+c = int(a)*int(b)
+d = int(a)+int(b)
+if c > 1000:
+    print(d)
+else:
+    print(c)
---
+++ score.py
@@ -0,0 +1,10 @@
+x = input()
+y = int(x)
+if y >= 85:
+    print('A')
+elif 70 <= y < 85:
+    print('B')
+elif 60 <= y < 70 :
+    print('c')
+else:
+    print('D')
